6f3019cf9fedea4e2ee917d70ea9faa93c7e13ba
Gerald Versluis <gerald.versluis@microsoft.com>
2024-09-24 11:14:01 +0200

    Add blank MAUI app

---
+++ src/Conference/Conference.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Conference.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/Conference/Conference.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Conference.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
