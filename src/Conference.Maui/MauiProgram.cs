ea31fd418095d2a7490ec3aa35147b6527774d38
Gerald Versluis <gerald.versluis@microsoft.com>
2024-09-26 21:18:07 +0200

    Add some simple styling

--- src/Conference.Maui/MauiProgram.cs
+++ src/Conference.Maui/MauiProgram.cs
@@ -18,6 +18,7 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Poppins-SemiBold.ttf", "PoppinsSemibold");
             })
             .UseMauiCommunityToolkit();
 
