3d23a814cb1c1a7d7619b4e7e8b2d0269be9dc45
Gerald Versluis <gerald@verslu.is>
2025-07-19 22:11:15 +0200

    Last feedback

--- src/Conference.Maui/Models/TimeHeader.cs
+++ src/Conference.Maui/Models/TimeHeader.cs
@@ -2,6 +2,13 @@ namespace Conference.Maui.Models;
 
 public class TimeHeader
 {
-    public string TimeDisplayText { get; set; } = string.Empty;
+    private string _timeDisplayText = string.Empty;
+    
+    public string TimeDisplayText 
+    { 
+        get => _timeDisplayText;
+        set => _timeDisplayText = value ?? string.Empty;
+    }
+    
     public int SessionCount { get; set; }
 }
